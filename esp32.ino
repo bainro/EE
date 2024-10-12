e9fad97141ce6711ed6bdf70acb9bf322f0e1b3d
Robert Bain <31903812+bainro@users.noreply.github.com>
2024-10-12 16:58:52 -0700

    Add files via upload

--- esp32.ino
+++ esp32.ino
@@ -1,10 +1,16 @@
 #include "WiFi.h"
+#include "HTTPClient.h"
 
 const char* ssid = "D3AB8E";
 const char* password =  "4CE156WJ01BBE";
 const char builtin_LED = 2;
 const int v_div_pin = 34;
 float batt_v = 0;
+bool is_recording = false;
+bool is_motion = false;
+uint16_t motion_cooldown = 5000; // 5 seconds
+uint32_t last_motion;
+String animalId = "unspecified_mouse";
 const byte PIR_pin = 2; // IR motion sensor
 String currentLine = ""; // holds incoming data from the client
 
@@ -12,6 +18,7 @@ NetworkServer server(80);
 
 void setup() {
   Serial.begin(115200);
+  last_motion = millis();
   pinMode(builtin_LED, OUTPUT);  // set the LED pin mode
   pinMode(v_div_pin, INPUT); // for reading battery voltage
   pinMode(PIR_pin, INPUT);
@@ -33,10 +40,25 @@ void setup() {
 }
 
 void loop() {
-  NetworkClient client = server.accept();  // listen for incoming clients
+  if (is_recording) {
+    is_motion = digitalRead(PIR_pin);
+    if (is_motion) {
+      if ((millis() - last_motion) > motion_cooldown) {
+        last_motion = millis();
+        Serial.println("MOTION DETECTED!");
+        HTTPClient http;
+        String urlWithParams = "http://192.168.0.2:8080/motion?animal_id=" + animalId;
+        http.begin(urlWithParams.c_str());
+        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        int response = http.GET();
+        http.end();
+      }
+    }
+  }
 
+  NetworkClient client = server.accept();  // listen for incoming clients
   if (client) {                     // if you get a client,
-    Serial.println("New Client.");  // print a message out the serial port
+    Serial.println("\nNew Client.");  // print a message out the serial port
     
     while (client.connected()) {    // loop while the client's connected
       if (client.available()) {     // if there's bytes to read from the client,
@@ -55,6 +77,9 @@ void loop() {
 
             // the content of the HTTP response follows the header:
             client.print("Click <a href='/battery'>here</a> to check battery.<br>");
+            client.print("Click <a href='/start'>here</a> to start recording motion.<br>");
+            client.print("Click <a href='/stop'>here</a> to stop recording motion.<br>");
+            client.print("Click <a href='/recording'>here</a> to check if recording.<br>");
 
             // The HTTP response ends with another blank line:
             client.println();
@@ -73,27 +98,58 @@ void loop() {
           client.println("HTTP/1.1 200 OK");
           client.println("Content-type:text/html");
           client.println();
-
-          // read the battery voltage pin
+          // read the battery voltage pin w/ 12bit precision & max 3.3V
           batt_v = ((float)analogRead(v_div_pin)) / 4095 * 3.3;
           // convert it to the true voltage given our voltage divider
           batt_v = batt_v * 3.13; // extra 13% from experimental measures
-
           // the content of the HTTP response follows the header:
-          client.print("Battery voltage is: " + String(batt_v, 2) + "V.<br>");
-          client.print("Click <a href='/battery'>here</a> to check the latest battery volatage.<br>");
-
-          bool is_motion = digitalRead(PIR_pin);
-          if (is_motion) {
-            client.println("MOTION DETECTED!");
-          }
-          else {
-            client.println("no motion detected ...");
-          }
-
+          client.print(String(batt_v, 2));
           // The HTTP response ends with another blank line:
           client.println();
-          digitalWrite(builtin_LED, HIGH);  // turn the LED on
+          break; // don't send default HTTP response
+        } else if (currentLine.endsWith("GET /recording")) {
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+          if (is_recording) {
+            client.println("true");
+          }
+          else {
+            client.println("false");
+          }
+          client.println();
+          break; // don't send default HTTP response
+        } else if (currentLine.endsWith("GET /start")) {
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+          is_recording = true;
+          client.println("acknowledged");
+          client.println();
+          break; // don't send default HTTP response
+        } else if (currentLine.endsWith("GET /stop")) {
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+          is_recording = false;
+          client.println("acknowledged");
+          client.println();
+          break; // don't send default HTTP response
+        } else if (currentLine.endsWith("GET /animal")) {
+          // set animal ID for logging motion events
+          c = client.read();
+          animalId = "";
+          // the rest of the requested URL is the animal ID
+          // delimited with a carriage return
+          while (c != '\r' && c != '\n') {
+            animalId += c;
+            c = client.read();
+          }
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+          client.println("acknowledged");
+          client.println();
           break; // don't send default HTTP response
         }
       }
