116064c68b05ab6586d18fa9089020e663e22980
Robert Bain <31903812+bainro@users.noreply.github.com>
2024-11-10 22:08:27 -0800

    Add files via upload

--- src/common/read_serial_RAD.m
+++ src/common/read_serial_RAD.m
@@ -10,5 +10,17 @@ function read_serial_RAD(obj,~,app)
         csv_path = fullfile(p1, p2);
         t = string(datetime());
         writematrix(t,csv_path,'WriteMode','append');
+    elseif contains(app.sdata, 'PLUGGED IN')
+        i = str2num(erase(app.sdata, "PLUGGED IN CAGE "));
+        name = app.animal_names(i);
+        disp("[matlab] cage #" + i + " (" + name + ") plugged in");
+        app.CageDropDown.Value = app.CageDropDown.Items{i};
+        app.change_cage_pub();
+    elseif contains(app.sdata, 'UNPLUGGED')
+        i = str2num(erase(app.sdata, "UNPLUGGED CAGE "));
+        name = app.animal_names(i);
+        disp("[matlab] cage #" + i + " (" + name + ") unplugged");
+        app.CageDropDown.Value = app.CageDropDown.Items{i};
+        app.change_cage_pub();
     end
 end
