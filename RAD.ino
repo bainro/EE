2eb5ac9b2c2181e308362f06addf3d3e7e037a08
Robert Bain <31903812+bainro@users.noreply.github.com>
2024-11-11 14:23:56 -0800

    Add files via upload

--- RAD.ino
+++ RAD.ino
@@ -25,16 +25,22 @@ uint32_t last_usb_check;
 bool is_connected = false;
 bool temp_usb = false;
 bool usb_1 = false;
+bool usb_2 = false;
+bool usb_3 = false;
+bool usb_4 = false;
 
 // cage USB status PINs
 const byte USB_1 = 7;
+const byte USB_2 = 8;
+const byte USB_3 = 9;
+const byte USB_4 = 10;
 
 void setup() {
   // Initialize Serial Connections
   Serial.begin(115200);         //  Initialize serial connection
   Wire.begin();                 //  join i2c bus (address optional for master)
-  Serial.println("RAD Arduino Nano #1 running");
-  last_usb_check = millis();
+  Serial.println("RAD Arduino Nano running");
+  
   last_motion_1 = last_motion_2 = last_motion_3 = millis();
   last_motion_3 = last_motion_4 = last_motion_5 = last_motion_6;
   
@@ -48,9 +54,31 @@ void setup() {
 
   // USB status PINs
   pinMode(USB_1, INPUT);
+  pinMode(USB_2, INPUT);
+  pinMode(USB_3, INPUT);
+  pinMode(USB_4, INPUT);
 
   // figure out which cages are plugged in at startup
   usb_1 = digitalRead(USB_1);
+  usb_2 = digitalRead(USB_2);
+  usb_3 = digitalRead(USB_3);
+  usb_4 = digitalRead(USB_4);
+  last_usb_check = millis();
+
+  // matlab assumes they're unplugged at startup
+  if (usb_4) {
+    Serial.println("PLUGGED IN CAGE 4");
+  }
+  if (usb_3) {
+    Serial.println("PLUGGED IN CAGE 3");
+  }
+  if (usb_2) {
+    Serial.println("PLUGGED IN CAGE 2");
+  }
+  // reverse order ends us on cage #1 in the matlab GUI
+  if (usb_1) {
+    Serial.println("PLUGGED IN CAGE 1");
+  }
 }
 
 void loop() {
@@ -68,6 +96,39 @@ void loop() {
         Serial.println("UNPLUGGED CAGE 1");
       }
     }
+
+    temp_usb = usb_2;
+    usb_2 = digitalRead(USB_2);
+    if (temp_usb != usb_2) {
+      if (usb_2) {
+        Serial.println("PLUGGED IN CAGE 2");
+      }
+      else {
+        Serial.println("UNPLUGGED CAGE 2");
+      }
+    }
+
+    temp_usb = usb_3;
+    usb_3 = digitalRead(USB_3);
+    if (temp_usb != usb_3) {
+      if (usb_3) {
+        Serial.println("PLUGGED IN CAGE 3");
+      }
+      else {
+        Serial.println("UNPLUGGED CAGE 3");
+      }
+    }
+
+    temp_usb = usb_4;
+    usb_4 = digitalRead(USB_4);
+    if (temp_usb != usb_4) {
+      if (usb_4) {
+        Serial.println("PLUGGED IN CAGE 4");
+      }
+      else {
+        Serial.println("UNPLUGGED CAGE 4");
+      }
+    }
   }
 
   // check cage #1
@@ -78,6 +139,39 @@ void loop() {
       if (usb_1) {
         last_motion_1 = millis();
         Serial.println("MOTION CAGE 1");
+      }
+    }
+  }
+
+  // check cage #2
+  is_motion = digitalRead(PIR_2);
+  if (is_motion) {
+    if ((millis() - last_motion_2) > motion_cooldown) {
+      if (usb_2) {
+        last_motion_2 = millis();
+        Serial.println("MOTION CAGE 2");
+      }
+    }
+  }
+
+  // check cage #3
+  is_motion = digitalRead(PIR_3);
+  if (is_motion) {
+    if ((millis() - last_motion_3) > motion_cooldown) {
+      if (usb_3) {
+        last_motion_3 = millis();
+        Serial.println("MOTION CAGE 3");
+      }
+    }
+  }
+
+  // check cage #4
+  is_motion = digitalRead(PIR_4);
+  if (is_motion) {
+    if ((millis() - last_motion_4) > motion_cooldown) {
+      if (usb_4) {
+        last_motion_4 = millis();
+        Serial.println("MOTION CAGE 4");
       }
     }
   }
